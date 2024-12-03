36f9374c51bff6a938501664a5ba633873413110
gabrielstenzowski <gabriel.stenzowskicav@gmail.com>
2024-12-03 15:58:41 -0300

    :sparkles: Feat-finishComponents

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,7 +1,51 @@
 'use client'
 
+import { usePathname } from 'next/navigation'
 import React from 'react'
-
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
+import { CiMenuFries } from 'react-icons/ci'
+import Link from 'next/link'
+const Links = [
+  { name: 'home', path: '/' },
+  { name: 'services', path: '/services' },
+  { name: 'resume', path: '/resume' },
+  { name: 'work', path: '/work' },
+  { name: 'contact', path: '/contact' },
+]
 export default function MobileNav() {
-  return <div>mobile nav</div>
+  const pathname = usePathname()
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-[32px] text-accent" />
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <div>logo</div>
+        <div className="mt-32 mb-40 text-center text-2xl">
+          <Link href="/">
+            <h1 className="text-4xl font-semibold">
+              Gabriel <span className="text-accent">.</span>
+            </h1>
+          </Link>
+        </div>
+
+        <nav className="flex flex-col justify-center items-center gap-8">
+          {Links.map((link, index) => {
+            return (
+              <Link
+                href={link.path}
+                key={index}
+                className={`${
+                  link.path === pathname &&
+                  'text-accent border-b-2 border-accent'
+                }text-xl capitalize hover:text-accent transition-all`}
+              >
+                {link.name}
+              </Link>
+            )
+          })}
+        </nav>
+      </SheetContent>
+    </Sheet>
+  )
 }
